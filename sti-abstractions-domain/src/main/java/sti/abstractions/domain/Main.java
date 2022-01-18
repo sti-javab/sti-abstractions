776167dc43c575177370825703f5d754b37f11c3
zgrkbr <ozgurkibar78@gmail.com>
2022-01-18 12:56:17 +0100

    add nullpointer to main method

--- sti-abstractions-domain/src/main/java/sti/abstractions/domain/Main.java
+++ sti-abstractions-domain/src/main/java/sti/abstractions/domain/Main.java
@@ -1,15 +1,20 @@
 package sti.abstractions.domain;
 
+import java.util.Scanner;
+
 public class Main {
 
     //@TODO make it a story!
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Owl oliver = new Owl(-10);
         System.out.println("olivers wingSpan: "+ oliver.getWingSpan());
         Squirrel squiggie = new Squirrel();
-        PineTree pine = new PineTree(12);
-        System.out.println(pine.pineCones.size());
+        //PineTree pine = new PineTree(12);
+        PineTree pine = null; //will result in NullPointerException
+
         squiggie.eat(pine.pineCones.get(0));
+
     }
 
 }
